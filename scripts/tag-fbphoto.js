e016bbaec291d3ec50f75471969cdf6c98e122cc
kong0107 <kong0107@gmail.com>
2017-04-11 18:05:25 +0800

    change dates of some posts

---
+++ scripts/tag-fbphoto.js
@@ -0,0 +1,24 @@
+'use strict';
+var hexoUtil = require('hexo-util');
+
+/**
+* @summary Facebook Post Tag without texts
+* @description FB JS SDK must be included in the front-end first and specified to the specified app ID. Otherwise this will be just a link.
+* @see {@link https://developers.facebook.com/docs/plugins/embedded-posts}
+* @example
+*   {% fbphoto link %}
+*/
+hexo.extend.tag.register('fbphoto', function(args) {
+    var link = args[0];
+    return hexoUtil.htmlTag("div", {
+        "class": "fb-post",
+        "data-href": link,
+        "data-show-text": "false"
+    }, hexoUtil.htmlTag("blockquote", {
+        "cite": link,
+        "class": "fb-xfbml-parse-ignore"
+    }, hexoUtil.htmlTag("a", {
+        "rel": "external",
+        "href": link
+    }, link)));
+});
