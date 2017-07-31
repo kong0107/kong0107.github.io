6fe7ca06e245b8a35c29cedf44fa638cc8f1ce17
Kong Kao <kong0107@gmail.com>
2017-07-31 23:54:30 +0800

    media_list.ejs supports preview

--- themes/cactus-dark/source/js/slideshow.js
+++ themes/cactus-dark/source/js/slideshow.js
@@ -1,18 +1,34 @@
-$(".slideshow").each(function() {
-    var counter = 0;
-    var images = $("img", this);
-    images.hide();
-    setInterval(function() {
-        images.hide();
-        $(images[counter]).show();
-        counter = (counter + 1) % images.length;
-    }, 1500);
-});
-
 $(".thumbnail-container").each(function() {
     var self = $(this);
+    var gallery = self.children(".gallery")[0];
+    var video = self.children("video")[0];
+    var intervalID = 0;
     self.children().not(".poster").hide();
     self.mouseover(function() {
-        console.log($("video", this));
+        if(video) {
+            $(video).show();
+            video.play();
+            return;
+        }
+        if(gallery) {
+            var photos = $(gallery).children("img");
+            $(gallery).show();
+            intervalID = setInterval(function() {
+                photos.hide();
+                var pi = (parseInt(gallery.dataset.posterIndex) + 1) % photos.length;
+                $(photos[pi]).show();
+                gallery.dataset.posterIndex = pi;
+            }, 1000);
+        }
+    });
+    self.mouseout(function() {
+        if(video) {
+            video.pause();
+            return;
+        }
+        if(gallery && intervalID) {
+            clearInterval(intervalID);
+            intervalID = 0;
+        }
     });
 });
