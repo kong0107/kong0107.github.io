f0ea0208edcb026e6f128abd3ddac9d53fb9da20
Kong Kao <kong0107@gmail.com>
2017-07-31 23:00:48 +0800

    updating media_list.ejs

--- themes/cactus-dark/source/js/slideshow.js
+++ themes/cactus-dark/source/js/slideshow.js
@@ -8,3 +8,11 @@ $(".slideshow").each(function() {
         counter = (counter + 1) % images.length;
     }, 1500);
 });
+
+$(".thumbnail-container").each(function() {
+    var self = $(this);
+    self.children().not(".poster").hide();
+    self.mouseover(function() {
+        console.log($("video", this));
+    });
+});
